4a975dfb1471c972ddc54b71be1ccbde524e6e97
EnKnight <rafael.roldan93@gmail.com>
2017-05-09 23:20:44 -0500

    Add files via upload

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -17,15 +17,19 @@ angular.module('proyectoFinalApp')
   });
 
     function Audio(tagname, file1, file2){
-  	if (!bandera){
-		document.getElementById(tagname).play();
-		document.getElementById('sound_img').src = "images/".concat(file1);
-		bandera=true;
+		var sound = document.getElementById(tagname);
+
+	  	if (!bandera){
+			sound.play();
+			document.getElementById('sound_img').src = "images/".concat(file1);
+			bandera=true;
+	  	}
+	  	
+		else {
+			sound.pause();
+			sound.currentTime = 0;
+			document.getElementById('sound_img').src = "images/".concat(file2);
+			bandera=false;
+	  	}
   	}
-	else {
-		document.getElementById(tagname).pause();
-		document.getElementById('sound_img').src = "images/".concat(file2);
-		bandera=false;
-	  }
-  }
 
